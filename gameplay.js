dd0f156f2146eaad7018a45d8352c4c67c60f11e
unknown <107675544+tbinhnoob@users.noreply.github.com>
2023-06-10 08:57:53 +0700

    board create

--- gameplay.js
+++ gameplay.js
@@ -1,5 +1,40 @@
-const boardDefault = {
-    cnt_row: 15,
-    cnt_col: 30,
-    cnt_bomb: 50,
+const gameBoard = document.getElementById("MinesweeperBoard");
+
+const board_info = {
+    cnt_row: 7,
+    cnt_col: 7
 };
+
+let state;
+let clicked;
+
+function hash(i, j) {
+    return (i - 1) * board_info.cnt_col + j - 1;
+}
+
+function init() {
+    state = new Array(board_info.cnt_row + 1);
+    for (var i = 1; i <= board_info.cnt_row; i++) {
+        state[i] = new Array(board_info.cnt_col + 1).fill(0);
+    }
+
+    clicked = new Array(board_info.cnt_row + 1);
+    for (var i = 1; i <= board_info.cnt_row; i++) {
+        clicked[i] = new Array(board_info.cnt_col + 1).fill(false);
+    }
+
+    var table = document.createElement("table");
+    var row, td;
+    for (var i = 1; i <= board_info.cnt_row; i++) {
+        row = document.createElement("tr");
+        for (var j = 1; j <= board_info.cnt_col; j++) {
+            td = document.createElement("td");
+            td.id = hash(i, j);
+            row.appendChild(td);
+            td.textContent = "";
+            // addCellListener(td, i, j);
+        }
+        table.appendChild(row);
+    }
+    gameBoard.appendChild(table);
+}
